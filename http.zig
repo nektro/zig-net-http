25fcee83673283e64641e7b3e304bf811ab32264
Meghan Denny <hello@nektro.net>
2026-05-22 03:26:20 -0700

    eliminate usingnamespace

--- http.zig
+++ http.zig
@@ -266,8 +266,22 @@ pub const ClientRequest = struct {
         }
     }
 
+    const R = nio.Readable(@This(), ._var);
+    pub const readAll = R.readAll;
+    pub const readAtLeast = R.readAtLeast;
+    pub const readNoEof = R.readNoEof;
+    pub const readAllAlloc = R.readAllAlloc;
+    pub const readArray = R.readArray;
+    pub const readByte = R.readByte;
+    pub const readUntilDelimiterArrayList = R.readUntilDelimiterArrayList;
+    pub const readUntilDelimiterAlloc = R.readUntilDelimiterAlloc;
+    pub const readUntilDelimitersBuf = R.readUntilDelimitersBuf;
+    pub const readUntilDelimitersArrayList = R.readUntilDelimitersArrayList;
+    pub const readAlloc = R.readAlloc;
+    pub const readInt = R.readInt;
+    pub const readUntilDelimitersAlloc = R.readUntilDelimitersAlloc;
+
     pub const ReadError = net.Stream.ReadError;
-    pub usingnamespace nio.Readable(@This(), ._var);
     pub fn read(req: *ClientRequest, buffer: []u8) ReadError!usize {
         return req.reader.read(buffer);
     }
